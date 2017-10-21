6749b36dadd59184b9cf9ea6ffde6c126bfbbf8d
Dan Shechter <dans@houmus.org>
2017-10-21 20:42:28 +0300

    reshuffle some stuff, run for given amount of time, do sync/async

--- BenchmarkDb/Program.cs
+++ BenchmarkDb/Program.cs
@@ -24,6 +24,9 @@ namespace BenchmarkDb
 
         static object synlock = new object();
 
+        static int _stopping = 0;
+        public static bool Stopping { get => _stopping == 1; set => Interlocked.Exchange(ref _stopping, value ? 1 : 0); }
+
         static void Main(string[] args)
         {
             System.Net.ServicePointManager.DefaultConnectionLimit = 1000;
@@ -63,42 +66,69 @@ namespace BenchmarkDb
                     break;
             }
 
-            Console.WriteLine($"Running with {args[0]} on {connectionString}");
+            var mode = args[3] == "async" ? "async" : "sync";
+            var delay = int.Parse(args[4]) * 1000;
+
+            Console.WriteLine($"Running with {args[0]} on {connectionString}, {mode} mode");
 
             var stopwatch = new Stopwatch();
-            var stopping = false;
             var startTime = DateTime.UtcNow;
+            var stopTime = DateTime.UtcNow;
             var lastDisplay = DateTime.UtcNow;
             var lastNewTask = DateTime.UtcNow;
+            var totalTransactions = 0;
+            var listOfResults = new List<double>(1000);
+            var lotsofbackspaces = String.Join("", Enumerable.Repeat("\b", 80));
+            var lotsofspaces = String.Join("", Enumerable.Repeat(" ", 80));
 
-            var tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWork, TaskCreationOptions.LongRunning).Unwrap()).ToList();
+            var tasks = mode == "async" ?
+               Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWork, TaskCreationOptions.LongRunning)).ToList() :
+               Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkAsync, TaskCreationOptions.LongRunning).Unwrap()).ToList();
 
             Task.Run(async () =>
             {
-                while (!stopping)
+                while (!Stopping)
                 {
                     await Task.Delay(1000);
                     var now = DateTime.UtcNow;
-
-                    Console.Write($"{tasks.Count} Threads, {Counter / (now - lastDisplay).TotalSeconds} tps                                   ");
-                    Console.CursorLeft = 0;
+                    var tps = Counter / (now - lastDisplay).TotalSeconds;
+                    totalTransactions += Counter;
+                    listOfResults.Add(tps);
+                    Console.Write(lotsofbackspaces);
+                    Console.Write($"{now.TimeOfDay}: {tasks.Count:D2} Threads, tps: {tps:F2}");
                     lastDisplay = now;
-                    Counter = 0;
+                    Interlocked.Exchange(ref Counter, 0);
                 }
-
+                
             });
 
-            Task.Run(() =>
+            Task.Run(async () =>
             {
-                Console.ReadLine();
-                stopping = true;
+                await Task.Delay(delay);
+                Stopping = true;
+                stopTime = DateTime.UtcNow;
+                await Task.Delay(2000);
             });
 
             Task.WhenAll(tasks).GetAwaiter().GetResult();
+            var totalTps = totalTransactions / (stopTime - startTime).TotalSeconds;
+            listOfResults.Remove(listOfResults.Max());
+            listOfResults.Remove(listOfResults.Min());
+            
+            Console.Write(lotsofbackspaces);
+            Console.Write(lotsofspaces);
+            Console.Write(lotsofbackspaces);
+            Console.WriteLine($"{tasks.Count:D2} Threads, tps: {totalTps:F2}, stddev(w/o best+worst): {CalculateStdDev(listOfResults, listOfResults.Count):F2}");
+            double CalculateStdDev(IEnumerable<double> values, int count) {
+                var avg = values.Sum() / count;
+                double sum = values.Sum(d => Math.Pow(d - avg, 2));
+                return Math.Sqrt((sum)/count);
+            }
 
-            async Task DoWork()
+ 
+            async Task DoWorkAsync()
             {
-                while (!stopping)
+                while (!Stopping)
                 {
                     Interlocked.Add(ref Counter, 1);
 
@@ -119,6 +149,53 @@ namespace BenchmarkDb
                             using (var reader = await command.ExecuteReaderAsync())
                             {
                                 while (await reader.ReadAsync())
+                                {
+                                    results.Add(new Fortune
+                                    {
+                                        Id = reader.GetInt32(0),
+                                        Message = reader.GetString(1)
+                                    });
+                                }
+                            }
+                        }
+
+                        if (results.Count() != 12)
+                        {
+                            throw new ApplicationException("Not 12");
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+
+            void DoWork()
+            {
+                while (!Stopping)
+                {
+                    Interlocked.Add(ref Counter, 1);
+
+                    try
+                    {
+                        var results = new List<Fortune>();
+
+                        using (var connection = factory.CreateConnection())
+                        {
+                            var command = connection.CreateCommand();
+                            command.CommandText = "SELECT id,message FROM fortune";
+
+                            connection.ConnectionString = connectionString;
+                            connection.Open();
+
+                            command.Prepare();
+
+                            using (var reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
                                 {
                                     results.Add(new Fortune
                                     {
