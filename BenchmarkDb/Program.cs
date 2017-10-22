b66c080735596ac9998f51ce34e3e9b27c35956f
Dan Shechter <dans@eagle-labs.co.il>
2017-10-22 19:58:12 +0300

    Support more bench mark modes, cleanup

--- BenchmarkDb/Program.cs
+++ BenchmarkDb/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace BenchmarkDb
 {
     class Program
     {
-        static volatile int Counter = 0;
+        static int _counter;
         static int NumTasks = 16;
 
         const string PostgreSql = nameof(PostgreSql);
@@ -66,8 +67,9 @@ namespace BenchmarkDb
                     break;
             }
 
-            var mode = args[3] == "async" ? "async" : "sync";
-            var delay = int.Parse(args[4]) * 1000;
+            var mode = args[3];
+            var seconds = int.Parse(args[4]);
+            var delay = seconds * 1000;
 
             Console.WriteLine($"Running with {args[0]} on {connectionString}, {mode} mode");
 
@@ -77,29 +79,49 @@ namespace BenchmarkDb
             var lastDisplay = DateTime.UtcNow;
             var lastNewTask = DateTime.UtcNow;
             var totalTransactions = 0;
-            var listOfResults = new List<double>(1000);
+            var listOfResults = new List<double>(seconds + 60);
             var lotsofbackspaces = String.Join("", Enumerable.Repeat("\b", 80));
             var lotsofspaces = String.Join("", Enumerable.Repeat(" ", 80));
 
-            var tasks = mode == "sync" ?
-               Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkSync, TaskCreationOptions.LongRunning)).ToList() :
-               Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkAsync, TaskCreationOptions.LongRunning).Unwrap()).ToList();
-
-            Task.Run(async () =>
+            List<Task> tasks = null;
+            switch (mode)
             {
+                case "sync":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkSync, TaskCreationOptions.LongRunning)).ToList();
+                    break;
+                case "sync+conn":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkSyncKeepConn, TaskCreationOptions.LongRunning)).ToList();
+                    break;
+                case "sync+conn+cmd":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkSyncKeepConnKeepCmd, TaskCreationOptions.LongRunning)).ToList();
+                    break;
+                case "async":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkAsync, TaskCreationOptions.LongRunning).Unwrap()).ToList();
+                    break;
+                case "async+conn":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkAsyncKeepConn, TaskCreationOptions.LongRunning).Unwrap()).ToList();
+                    break;
+                case "async+conn+cmd":
+                    tasks = Enumerable.Range(1, NumTasks).Select(_ => Task.Factory.StartNew(DoWorkAsyncKeepConnKeepCmd, TaskCreationOptions.LongRunning).Unwrap()).ToList();
+                    break;
+                default:
+                    throw new Exception($"Unknown benchmark mode {mode} requesgted");
+
+            }
+            var reporterTask =
+                Task.Run(async () => {
                 while (!Stopping)
                 {
                     await Task.Delay(1000);
                     var now = DateTime.UtcNow;
-                    var tps = Counter / (now - lastDisplay).TotalSeconds;
-                    totalTransactions += Counter;
+                    var tps = _counter / (now - lastDisplay).TotalSeconds;
+                    totalTransactions += _counter;
                     listOfResults.Add(tps);
                     Console.Write(lotsofbackspaces);
                     Console.Write($"{now.TimeOfDay}: {tasks.Count:D2} Threads, tps: {tps:F2}");
                     lastDisplay = now;
-                    Interlocked.Exchange(ref Counter, 0);
+                    Interlocked.Exchange(ref _counter, 0);
                 }
-                
             });
 
             Task.Run(async () =>
@@ -111,39 +133,138 @@ namespace BenchmarkDb
             });
 
             Task.WhenAll(tasks).GetAwaiter().GetResult();
+            reporterTask.GetAwaiter().GetResult();
+
             var totalTps = totalTransactions / (stopTime - startTime).TotalSeconds;
+
             listOfResults.Remove(listOfResults.Max());
             listOfResults.Remove(listOfResults.Min());
-            
+            var stddev = CalculateStdDev(listOfResults, listOfResults.Count);
             Console.Write(lotsofbackspaces);
             Console.Write(lotsofspaces);
             Console.Write(lotsofbackspaces);
-            Console.WriteLine($"{tasks.Count:D2} Threads, tps: {totalTps:F2}, stddev(w/o best+worst): {CalculateStdDev(listOfResults, listOfResults.Count):F2}");
-            double CalculateStdDev(IEnumerable<double> values, int count) {
-                var avg = values.Sum() / count;
-                double sum = values.Sum(d => Math.Pow(d - avg, 2));
-                return Math.Sqrt((sum)/count);
+            Console.WriteLine($"{tasks.Count:D2} Threads, tps: {totalTps:F2}, stddev(w/o best+worst): {stddev:F2}");
+
+            using (var sw = File.AppendText("results.md"))
+            {
+                sw.WriteLine($"|${mode}|${tasks.Count}|{totalTps:F2}|{stddev}|");
             }
 
- 
+            double CalculateStdDev(IEnumerable<double> values, int count)
+            {
+                var avg = values.Sum() / count;
+                double sum = values.Sum(d => Math.Pow(d - avg, 2));
+                return Math.Sqrt((sum) / count);
+            }
+
+
             async Task DoWorkAsync()
+            {
+                var results = new List<Fortune>();
+                while (!Stopping)
+                {
+                    Interlocked.Add(ref _counter, 1);
+
+                    try
+                    {
+                        using (var connection = factory.CreateConnection())
+                        {
+                            connection.ConnectionString = connectionString;
+                            await connection.OpenAsync();
+                            using (var command = connection.CreateCommand())
+                            {
+                                command.CommandText = "SELECT id,message FROM fortune";
+                                command.Prepare();
+
+                                using (var reader = await command.ExecuteReaderAsync())
+                                {
+                                    while (await reader.ReadAsync())
+                                    {
+                                        results.Add(new Fortune
+                                        {
+                                            Id = reader.GetInt32(0),
+                                            Message = reader.GetString(1)
+                                        });
+                                    }
+                                }
+                            }
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
+                        }
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                } // Stopping
+            }
+
+
+            async Task DoWorkAsyncKeepConn()
             {
                 var results = new List<Fortune>();
                 using (var connection = factory.CreateConnection())
                 {
-                            connection.ConnectionString = connectionString;
-                            await connection.OpenAsync();
-                            using (var command = connection.CreateCommand()) {
-                            command.CommandText = "SELECT id,message FROM fortune";
-                            command.Prepare();
-                while (!Stopping)
-                {
-                    Interlocked.Add(ref Counter, 1);
-
-                    try
+                    connection.ConnectionString = connectionString;
+                    await connection.OpenAsync();
+                    while (!Stopping)
                     {
+                        Interlocked.Add(ref _counter, 1);
 
+                        try
+                        {
+                            using (var command = connection.CreateCommand())
+                            {
+                                command.CommandText = "SELECT id,message FROM fortune";
+                                command.Prepare();
 
+                                using (var reader = await command.ExecuteReaderAsync())
+                                {
+                                    while (await reader.ReadAsync())
+                                    {
+                                        results.Add(new Fortune
+                                        {
+                                            Id = reader.GetInt32(0),
+                                            Message = reader.GetString(1)
+                                        });
+                                    }
+                                }
+                            }
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    } // Stopping
+                } // Connection
+            }
+
+            async Task DoWorkAsyncKeepConnKeepCmd()
+            {
+                var results = new List<Fortune>();
+                using (var connection = factory.CreateConnection())
+                using (var command = connection.CreateCommand())
+                {
+                    connection.ConnectionString = connectionString;
+                    await connection.OpenAsync();
+                    command.CommandText = "SELECT id,message FROM fortune";
+                    command.Prepare();
+                    while (!Stopping)
+                    {
+                        Interlocked.Add(ref _counter, 1);
+
+                        try
+                        {
                             using (var reader = await command.ExecuteReaderAsync())
                             {
                                 while (await reader.ReadAsync())
@@ -156,40 +277,129 @@ namespace BenchmarkDb
                                 }
                             }
 
-                        if (results.Count() != 12)
-                        {
-                            throw new ApplicationException("Not 12");
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
                         }
-                        results.Clear();
-                    }
 
-                    catch (Exception e)
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    } // Stopping
+                }
+            }
+            void DoWorkSync()
+            {
+                var results = new List<Fortune>();
+
+                    while (!Stopping)
                     {
-                        Console.WriteLine(e.Message);
+                        Interlocked.Increment(ref _counter);
+
+                        try
+                        {
+                            using (var connection = factory.CreateConnection())
+                            {
+                                connection.ConnectionString = connectionString;
+                                connection.Open();
+                                using (var command = connection.CreateCommand())
+                                {
+                                    command.CommandText = "SELECT id,message FROM fortune";
+                                    command.Prepare();
+                                    using (var reader = command.ExecuteReader())
+                                    {
+                                        while (reader.Read())
+                                        {
+                                            results.Add(new Fortune
+                                            {
+                                                Id = reader.GetInt32(0),
+                                                Message = reader.GetString(1)
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
-                } // Stopping
-                } // Command
-                } // Connection
             }
 
-
-            void DoWorkSync()
+            void DoWorkSyncKeepConn()
             {
                 var results = new List<Fortune>();
                 using (var connection = factory.CreateConnection())
                 {
                     connection.ConnectionString = connectionString;
                     connection.Open();
-                            using (var command = connection.CreateCommand()) { 
-                            command.CommandText = "SELECT id,message FROM fortune";
-                            command.Prepare();
-                while (!Stopping)
-                {
-                    Interlocked.Add(ref Counter, 1);
 
-                    try
+                    while (!Stopping)
                     {
+                        Interlocked.Increment(ref _counter);
 
+                        try
+                        {
+                            using (var command = connection.CreateCommand())
+                            {
+                                command.CommandText = "SELECT id,message FROM fortune";
+                                command.Prepare();
+                                using (var reader = command.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        results.Add(new Fortune
+                                        {
+                                            Id = reader.GetInt32(0),
+                                            Message = reader.GetString(1)
+                                        });
+                                    }
+                                }
+                            }
+
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                } // Connection
+            }
+
+
+            void DoWorkSyncKeepConnKeepCmd()
+            {
+                var results = new List<Fortune>();
+                using (var connection = factory.CreateConnection())
+                using (var command = connection.CreateCommand())
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    command.CommandText = "SELECT id,message FROM fortune";
+                    command.Prepare();
+                    while (!Stopping)
+                    {
+                        Interlocked.Increment(ref _counter);
+
+                        try
+                        {
                             using (var reader = command.ExecuteReader())
                             {
                                 while (reader.Read())
@@ -202,24 +412,23 @@ namespace BenchmarkDb
                                 }
                             }
 
-                        if (results.Count() != 12)
-                        {
-                            throw new ApplicationException("Not 12");
-                        }
-                        results.Clear();
+                            if (results.Count() != 12)
+                            {
+                                throw new ApplicationException("Not 12");
+                            }
+                            results.Clear();
 
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
                 }
-                        } // Command
-                        } // Connection
             }
         }
 
-        
+
     }
 
     //class Program
@@ -311,7 +520,7 @@ namespace BenchmarkDb
     //                if (tasks.Count <= MaxTasks)
     //                {
     //                    tasks.Add(Task.Run(Thing));
-    //                }                   
+    //                }
 
     //                async Task Thing()
     //                {
@@ -363,7 +572,7 @@ namespace BenchmarkDb
     //            }
     //        }
 
-    //        Task.WhenAll(tasks).GetAwaiter().GetResult();            
+    //        Task.WhenAll(tasks).GetAwaiter().GetResult();
     //    }
     //}
 
